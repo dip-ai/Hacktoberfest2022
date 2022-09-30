ff6dddb930bd2a8aec06b7c29cba8f218aeb1b45
Dipanjan Saha <79708282+dip-ai@users.noreply.github.com>
2022-09-30 23:55:08 +0530

    Kedane's Algorithm

---
+++ Data Structure And Algorithms/Kadane.java
@@ -0,0 +1,24 @@
+//Kadane's Algorithm
+// Given an integer array, find the contiguous subarray (containing at least one number) with the largest sum or in other words the maximum sum contiguous subarray and print its sum.
+// This code is contributed by dip-ai
+
+public class Kadane {
+    public static int KA(int[] Array) {
+        int currentNumber=0;
+        int maxNumber=0;
+        for(int i=0; i<Array.length; i++){
+            currentNumber=currentNumber+Array[i];
+            if(currentNumber>maxNumber){
+                maxNumber=currentNumber;
+            }
+            else if(currentNumber<0){
+                currentNumber=0;
+            }
+        }
+        return maxNumber;
+    }  
+    public static void main(String[] args) {
+        int Array[]={5,-6,2,6,5,-10};
+        System.out.println(KA(Array)); 
+    } 
+}
